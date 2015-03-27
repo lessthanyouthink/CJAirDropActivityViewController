11367f3ef766be6ee3ecc95a5ef33a588367a0b2
Charles Joseph <cljoseph@gmail.com>
2015-03-27 16:52:12 -0400

    Create CJAirDropActivityViewController.h

---
+++ CJAirDropActivityViewController.h
@@ -0,0 +1,11 @@
+//
+//  CJAirDropActivityViewController.h
+//
+//  Created by Charles Joseph on 2015-03-20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CJAirDropActivityViewController : UIActivityViewController
+
+@end
